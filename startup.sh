e85a5cbe8dc5545709d4e23f40658021a119b940
Marnu <marnu@lombard>
2016-06-25 10:05:26 +0200

    Add volumes for www, data & conf

--- startup.sh
+++ startup.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-docker run -d --name="marnu" -u root -p 242:22 -p 80:80 -p 3306:3306 -v /var/www/:/var/www/ -i -t marnu/ubuntu:latest
+docker run -d \
+  --name="marnu" \
+  -p 242:22 \
+  -p 80:80 \
+  -p 3306:3306 \
+  -v /Users/marnu/Desktop/Work/:/var/www/ \
+  -v `pwd`/data/:/data/ \
+  -v `pwd`/conf/:/conf/ \
+  -i -t marnu/ubuntu:latest
