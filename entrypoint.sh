78b6e897cbecb15c3256213cd3a7a36014a128cc
Marnu <marnu@lombard>
2016-06-25 10:06:07 +0200

    Fill out entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-# dunno?
+
+# As per MDS tech's docker set up
+if [ -f /var/www/000-default.conf ]; then
+	a2dissite 000-default
+fi
+for file in `find "/var/www/" -maxdepth 2 -name "*.conf`; do
+	ln -sf "$file" "/etc/apache2/sites-enabled/"
+done;
+
+service apache2 startup
+service mysql startup
